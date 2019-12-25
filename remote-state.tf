dd1101e9a26f2e80158664a5fd89119d50c46d7f
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2019-12-25 14:30:18 +0000

    remove temp file

--- remote-state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-cicd-poc"
-    storage_account_name = "stcicdpoc"
-    container_name       = "terraform-state"
-    key                  = "poc"
-  }
-}
